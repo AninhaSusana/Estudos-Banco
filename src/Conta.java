4db70262d7c77de604350de7ac3edcec419f8e4e
AninhaSusana <152209884+AninhaSusana@users.noreply.github.com>
2024-03-26 17:03:47 -0600

    atualizaçao

--- src/Conta.java
+++ src/Conta.java
@@ -1,9 +1,19 @@
-public class conta {
+public class Conta {
     private String numerodaConta; 
     private double saldo; 
     private String Saque; 
-    
+     
+        double limite; 
 
-}
+        void depositar(double valor){
+            saldo = saldo + valor; 
+        }
+        void sacar(double valor){
+            saldo = saldo - valor;
+        }
+    
+    }
+
+
 
 
