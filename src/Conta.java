77e4048796a4a396771e58d134a6880eab89bbf6
AninhaSusana <152209884+AninhaSusana@users.noreply.github.com>
2024-04-05 17:07:01 -0600

    a

--- src/Conta.java
+++ src/Conta.java
@@ -1,9 +1,62 @@
 public class Conta {
-    private String numerodaConta; 
+    private int numero; 
+    private String titular; 
     private double saldo; 
-    private String Saque; 
-     
-        double limite; 
+
+    static int qtdContas =  0; 
+
+    public Conta (String pTitular) {
+        this.titular = pTitular; 
+        qtdContas++; 
+        this.numero = qtdContas; 
+        this.saldo = 0; 
+
+    }
+    public void  DadosBancarios (){
+        System.out.println("");
+        System.out.println(" Dados Bancarios");
+        System.out.println("- Numero:" + this.numero);
+        System.out.println("- Titular"   + this.titular);
+        System.out.println(  "Saldo"       + this.saldo);
+
+        System.out.println("Fim Dados Bancarios");
+        System.out.println("");
+    }
+
+        public void Depositar( double pValor) {
+            System.out.println("");
+            System.out.println(" --- Realizando Deposito --- ");
+            System.out.println("Saldo Anterior" + this.saldo);
+
+
+            this.saldo += pValor; // this.saldo = this.saldo + pValor;
+
+            System.out.println(" --- Saldo Posterior:" + this.saldo );
+            System.out.println(" --- Fim do Deposito----");
+            System.out.println("");
+
+        }
+
+        public void Sacar ( double pValor) {
+            System.out.println("");
+            System.out.println(" --- Realizando Saque ----  ");
+            System.out.println(" --- Saldo Anterior:" + this.saldo );
+
+            if(pValor <= this.saldo){ // caso eu tenha saldo 
+                this.saldo -= pValor;
+                System.out.println(" --- Saldo Posterior: " + this.saldo );
+            }
+            else { // caso eu não tenha nada de saldo
+               System.out.println("Saldo Insuficiente "); 
+            }
+
+            System.out.println(" --- Fim do Saque --- ");
+            System.out.println("");
+       
+         
+
+        }
+
 
 }
 
--- src/ContaUnica.java
+++ src/ContaUnica.java
@@ -1,8 +1,54 @@
-public class ContaUnica {
-    private String numerodaConta; 
-    private double saldo; 
-    private String Saque; 
-     
-        double limite; 
+import java.util.Scanner;
 
+public class ContaUnica{
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner ( System.in ); 
+
+        String aux;
+        int opcao;
+        double valor; 
+
+        System.out.println("Digite o titular da conta");
+        aux = entrada.nextLine();
+
+
+        Conta c1 = new Conta (aux); 
+
+        do{
+             System.out.println("");
+            System.out.println(" --- MENU --- ");
+            System.out.println("1)Dados Bancarios");
+            System.out.println("2)Deposito ");
+            System.out.println("3) Saque ");
+            System.out.println("0) Sair");
+
+            System.out.print(" Opção: ");
+           opcao = entrada.nextInt();
+
+                if(opcao == 1){
+                    c1.DadosBancarios();
+                }
+                else if(opcao == 2 ){
+                    System.out.println("Digite o valor do Deposito: ");
+                    valor = entrada.nextDouble();
+
+            c1.Depositar(valor);
+           }
+           else if(opcao == 3 ){
+            System.out.println("Digite o valor do Deposito: ");
+            valor = entrada.nextDouble();
+
+            c1.Sacar(valor);
+           }
+           else if(opcao !=0){
+            System.out.println("Opção Invalida: ");
+           }
+
+           System.out.println("");
+
+
+
+        } while(opcao != 0 );
+
+    }
 }
